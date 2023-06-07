958511266765e37542f273d8f5292dc9426d8ceb
Mehedi <developermehedi128@gmail.com>
2023-06-07 16:35:47 +0600

    mongodb setupt and user post api added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -11,6 +12,59 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k8bloyi.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const fluentDb = client.db('fluendDB');
+        const userCollections = fluentDb.collection('users');
+
+        // user Api here
+        app.post('/user', async (req, res) => {
+            const userData = req.body;
+
+            const query = { email: userData.email };
+            const existingUser = await userCollections.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'User already exists' });
+            }
+
+            const result = await userCollections.insertOne(userData);
+            res.send(result);
+        });
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 
 app.get('/', async (req, res) => {
